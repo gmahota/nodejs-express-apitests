c94dd2945c961cbf6df59741562bcc5858f5834c
Guimarães Mahota <guimaraesmahota@gmail.com>
2020-09-04 16:34:51 +0200

    add images magic

--- server.js
+++ server.js
@@ -1,42 +1,44 @@
-const express = require('express' );
+const express = require("express");
 const set_paymentData = require("./services/mpesa.js");
-const robots = require( "./services/robots/index.js");
-const state = require('./services/robots/state.js')
+const robots = require("./services/robots/index.js");
+const state = require("./services/robots/state.js");
 
-const app = express()
-const port = 4000
+const app = express();
+const port = 4000;
 
-app.get('/', (req, res) => {
-  robots.image();
-    res.send('Hello World!')
-  });
+app.get("/", async (req, res) => {
+  await robots.video();
+  res.send("Hello World!");
+});
 
-  app.get('/mpesa/payment/:number-:reference-:transaction-:amount',async (req, res) => {
-    
+app.get(
+  "/mpesa/payment/:number-:reference-:transaction-:amount",
+  async (req, res) => {
     var number = req.params.number;
     var reference = req.params.reference;
     var transaction = req.params.transaction;
     var amount = req.params.amount;
 
-    await set_paymentData(number,reference,transaction,amount);
-  });
+    await set_paymentData(number, reference, transaction, amount);
+  }
+);
 
-  app.get('/robot/:textsearch', async (req, res) => {
-    const content = {
-      searchTerm : req.params.textsearch,
-      lang : "pt",
-      maximumSentences: 7
-    };
+app.get("/robot/:textsearch", async (req, res) => {
+  const content = {
+    searchTerm: req.params.textsearch,
+    lang: "pt",
+    maximumSentences: 7,
+  };
 
-    state.save(content);
+  state.save(content);
 
-    await robots.text(content);
-    
-    robots.image();
+  await robots.text(content);
 
-    res.send(content.sourceContentOriginal);
-  });
+  await robots.image();
+
+  res.send(content.sourceContentOriginal);
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 });
